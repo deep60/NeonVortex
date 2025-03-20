98294d5287f822827d28d1285f0cda14296f2c3e
deep60 <deep6060p@gmail.com>
2025-03-20 09:04:29 +0530

    Initial Commit

---
+++ NeonVortex/NeonVortexApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonVortexApp.swift
+//  NeonVortex
+//
+//  Created by P Deepanshu on 20/03/25.
+//
+
+import SwiftUI
+
+@main
+struct NeonVortexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeonVortexTests/NeonVortexTests.swift
@@ -0,0 +1,17 @@
+//
+//  NeonVortexTests.swift
+//  NeonVortexTests
+//
+//  Created by P Deepanshu on 20/03/25.
+//
+
+import Testing
+@testable import NeonVortex
+
+struct NeonVortexTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
